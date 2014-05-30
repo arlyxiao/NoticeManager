d82679e9761810b9ec1a9c400c0688e47af72551
mykingla <kingla_pei@163.com>
2014-05-30 14:37:36 +0800

    rename service receiver

--- SampleClient/src/com/example/sampleclient/StartMessageNoticeServiceAtBootReceiver.java
+++ SampleClient/src/com/example/sampleclient/StartMessageNoticeServiceAtBootReceiver.java
@@ -5,7 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class StartMyServiceAtBootReceiver extends BroadcastReceiver {
+public class StartMessageNoticeServiceAtBootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
