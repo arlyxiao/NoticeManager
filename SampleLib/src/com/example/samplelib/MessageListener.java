5f06793f7cc7fbf60fe12a9f1c1f466e5ebbbe56
mykingla <kingla_pei@163.com>
2014-06-05 16:09:48 +0800

    Add maven project

--- SampleLib/src/com/example/samplelib/MessageListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.samplelib;
-
-
-import android.app.PendingIntent;
-
-public interface MessageListener {
-    public PendingIntent build_pending_intent(String message_response);
-    public void build_notification(PendingIntent p_intent, String message_response);
-}
