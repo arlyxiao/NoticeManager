2e62a2dc28649544c8b325e390bbc81f408925eb
mykingla <kingla_pei@163.com>
2014-06-06 14:57:27 +0800

    remve SampleApp

--- SampleApp/noticelib/src/main/java/com/message/noticemanager/noticelib/MessageListener.java
+++
@@ -1,9 +0,0 @@
-package com.message.noticemanager.noticelib;
-
-
-import android.app.PendingIntent;
-
-public interface MessageListener {
-    public PendingIntent build_pending_intent(String message_response);
-    public void build_notification(PendingIntent p_intent, String message_response);
-}
