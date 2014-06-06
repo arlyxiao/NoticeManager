e286cc8e7deb83614e9dcdf310f645df77a595f3
mykingla <kingla_pei@163.com>
2014-06-06 10:38:57 +0800

    reafctor server

--- sample-server/app/controllers/home_controller.rb
+++ sample-server/app/controllers/home_controller.rb
@@ -1,23 +1,43 @@
 class HomeController < ApplicationController
+  layout false
+
 
   def index
 
     token = 'thisisforrepeatnotice'
+    message_json = {:title => '测试 title', :desc => '测试 desc', :other => '测试 other'}
+    a_message = token + message_json[:title] + message_json[:desc] + message_json[:other]
     
-    if params[:token] == token
+    if !has_token_and_message?(a_message)
       title = '测试 title'
       desc = '测试 desc'
       other = '测试 other'
 
-      render :json => {
-        :title => title,
-        :desc => desc,
-        :other => other
-      }
+      render :json => message_json
     end
 
-
+    render :nothing => true
     
+  end
+
+
+  def has_token_and_message?(a_message)
+    filename = 'token_and_message'
+    open(filename, 'a+') do |f|
+      f.each_line do |ff|
+        ff = ff.tr("\n","")
+        p ff
+        p a_message
+        while a_message == ff
+          p '此消息已经发送过'
+          return true
+        end
+      end
+
+      
+      f.puts a_message
+      false
+    end
   end
 
 end
