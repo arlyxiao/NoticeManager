944f44b93e62c05c3d30428202ddcd2c9674627f
mykingla <kingla_pei@163.com>
2014-06-09 16:02:45 +0800

    remove target dir

--- notice_manager/notice_manager-samples/target/unpack/apklibs/com.mindpin.android_notice_manager_apklib_0.1/src/com/mindpin/android/noticemanager/MessageListener.java
+++
@@ -1,8 +0,0 @@
-package com.mindpin.android.noticemanager;
-
-
-import android.app.PendingIntent;
-
-public interface MessageListener {
-    public PendingIntent build_pending_intent(String message_response);
-}
